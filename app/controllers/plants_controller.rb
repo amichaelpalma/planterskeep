36e77336bdd353b0a2b01d2c2e318d4eeafe4596
amichaelpalma <amichaelpalma@gmail.com>
2019-01-27 18:48:38 +0800

    update plant controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
 
+  before_action :set_plant, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @plants = Plant.all
   end
@@ -17,10 +19,36 @@ class PlantsController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @plant.update(plant_params)
+      flash[:notice] = 'Plant details updated.'
+      redirect_to plant_path(@plant)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @plant.destroy
+    flash[:alert] = 'Plant was successfully deleted.'
+    redirect_to root_path
+  end
+
 
   private
 
   def plant_params
     params.require(:plant).permit(:common_name, :scientific_name, :description)
+  end
+
+  def set_plant
+    @plant = Plant.find(params[:id])
+  end
 end

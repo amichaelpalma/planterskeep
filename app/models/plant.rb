aa8e19e1236c86da213cb5d900796b7c46052c40
amichaelpalma <amichaelpalma@gmail.com>
2019-01-31 13:39:43 +0800

    add scope with_eager_loaded and with_preloaded in plant model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,7 @@
 class Plant < ApplicationRecord
+
+  scope :with_eager_loaded_image, -> { eager_load(image_attachment: :blob) }
+  scope :with_preloaded_image, -> { preload(image_attachment: :blob) }
+  
+  has_one_attached :image
 end

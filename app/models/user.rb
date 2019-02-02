9832758f73e124e46a02422b5d194dce3ff03181
amichaelpalma <amichaelpalma@gmail.com>
2019-02-02 09:07:55 +0800

    allow login via username or email, remove ability to register email format as username, add validation and make login details case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,37 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+    conditions[:email].downcase! if conditions[:email]
+    where(conditions.to_h).first
+    
+    # when allowing distinct User records with, e.g., "username" and "UserName"...
+    where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+  end
+
+
+  # Only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
 end
